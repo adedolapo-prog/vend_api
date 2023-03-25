6922bae0aecd8fce9f5c9b44f6bbae94f6b02b1e
adedolapo-prog <shayne.doly94@gmail.com>
2023-03-25 14:46:55 +0100

    initial commit

--- src/core/server.ts
+++ src/core/server.ts
@@ -1,8 +1,6 @@
 import { application, app } from "./app"
-import connectToDatabase from "./db"
 import * as dotenv from "dotenv"
 import path from "path"
-import { loanRepaymentCRON } from "../utils/cron"
 
 dotenv.config({ path: path.join(__dirname, "../../.env") })
 
@@ -10,8 +8,6 @@ const PORT = process.env.PORT || 5000
 
 export const startServer = async () => {
   application() //instantiate express app
-  await connectToDatabase()
-  loanRepaymentCRON()
 
   app.listen(PORT, () => {
     console.log(`Application running on port ${PORT}`)
